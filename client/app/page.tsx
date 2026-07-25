06d8337d202b4d1e4b19d0ab51195f3d1aa68e7b
akshatp17 <akshatp0505@gmail.com>
2026-07-25 14:17:30 +0530

    auth page prototyped

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -3,11 +3,14 @@ import Authenticate from "@/components/authComponent";
 export default function Home() {
     return (
         <>
-            <div className="w-full h-screen flex flex-col justify-center items-center">
-                <p>TODO APP</p>
+            <div className="w-full h-screen flex flex-col justify-evenly items-center">
+                <div className="flex flex-col justify-center items-center">
+                    <p className="font-bold text-4xl">TODO APP</p>
+                    <p className="font-semibold text-gray-400">Manage Your Tasks</p>
+                </div>
 
                 {/* Login/Signup columm */}
-                <Authenticate></Authenticate>
+                <Authenticate />
             </div>
         </>
     );
--- client/components/authComponent.tsx
+++ client/components/authComponent.tsx
@@ -18,42 +18,53 @@ export default function Authenticate() {
     const { register, handleSubmit, watch, formState: { errors }, } = useForm()
 
     const onSubmit: SubmitHandler<authData> = (data) => {
-        console.log(data)
-        redirect('/home')
+        if (isLogin) {
+            console.log(data)
+        }
+        else {
+            console.log(data)
+            redirect('/home')
+        }
     }
 
     return (
         <div className="w-[50%] min-h-[50%] bg-gray-50 rounded-4xl flex flex-col justify-between items-center">
             <h1 className="">{isLogin ? "Login" : "Register"}</h1>
             <form onSubmit={handleSubmit(onSubmit)} className="flex flex-col justify-center items-center gap-2">
-                <p className="w-full">
+                <p className="w-full flex justify-between flex-col">
                     <span>Email : </span>
-                    <input type="email" name="email" id="email" className="border rounded-4xl" />
+                    <input type="email" id="email" className="border border-gray-300 rounded-xs  px-2 py-1" {...register("email", { required: true })} placeholder="Enter email" />
                 </p>
-                <p className="w-full">
-                    <span>Name : </span>
-                    <input type="text" className="border rounded-4xl" />
-                </p>
-                <p className="w-full">
+                {!isLogin ?
+                    <p className="w-full flex justify-between flex-col">
+                        <span>Name : </span>
+                        <input type="text" className="border border-gray-300 rounded-xs px-2 py-1" {...register("uname", { required: true })} placeholder="Enter your name" />
+                    </p>
+                    : ""
+                }
+                <p className="w-full flex justify-between flex-col">
                     <span>Password : </span>
-                    <input type="password" name="passwrd" id="" className="border rounded-4xl" />
+                    <input type="password" id="" className="border border-gray-300 rounded-xs px-2 py-1" {...register("pwrd", { required: true })} placeholder="Enter password" />
                 </p>
-                <p className="w-full">
-                    <span>Confirm Password : </span>
-                    <input type="password" name="cnfrmpasswrd" id="" className="border rounded-4xl" />
-                </p>
+                {!isLogin ?
+                    <p className="w-full flex justify-between flex-col">
+                        <span>Confirm Password : </span>
+                        <input type="password" id="" className="border border-gray-300 rounded-xs px-2 py-1" {...register("cnfrm_pwrd", { required: true })} placeholder="Confirm password" />
+                    </p>
+                    : ""
+                }
 
                 <input type="submit" value={isLogin ? "Login" : "Register"} className="bg-blue-500 px-5 py-1 rounded-full text-white cursor-pointer hover:scale-[1.05]" />
             </form>
 
-            <div>
+            <div className="text-[13px]">
                 {isLogin ?
                     <>
-                        Don't have an account? <button onClick={() => { setIsLogin(false) }} className="cursor-pointer">Register Now</button>
+                        Don't have an account? <button onClick={() => { setIsLogin(false) }} className="cursor-pointer text-blue-500">Register Now</button>
                     </>
                     :
                     <>
-                        Already have an account? <button onClick={() => { setIsLogin(true) }} className="cursor-pointer">Login Now</button>
+                        Already have an account? <button onClick={() => { setIsLogin(true) }} className="cursor-pointer text-blue-500">Login Now</button>
                     </>
                 }
             </div>
