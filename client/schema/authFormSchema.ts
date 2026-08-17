2d82c0accb163b56f38bc3b1fb9758c8a5b208e4
akshatp10 <akshat@thealteroffice.com>
2026-08-17 11:25:27 +0530

    fix(AuthForms) : Reduced redundant code and used omit in schema

--- client/schema/authFormSchema.ts
+++ client/schema/authFormSchema.ts
@@ -1,6 +1,6 @@
 import { z } from "zod";
 
-export const loginSchema = z.object({
+const authSchema = z.object({
 	email: z.email("Please enter a valid email address"),
 
 	name: z
@@ -9,24 +9,19 @@ export const loginSchema = z.object({
 		.min(2, "Name must be at least 2 characters")
 		.max(50, "Name cannot exceed 50 characters"),
 
-	password: z.string().min(6, "Password should be atleast 6 characters"),
+	password: z.string().min(6, "Password should be at least 6 characters"),
+
+	confirmPassword: z.string().min(1, "Please confirm your password"),
 });
 
-export const registerSchema = z
-	.object({
-		email: z.email("Please enter a valid email address"),
+export const loginSchema = authSchema.omit({
+	confirmPassword: true,
+});
 
-		name: z
-			.string()
-			.trim()
-			.min(2, "Name must be at least 2 characters")
-			.max(50, "Name cannot exceed 50 characters"),
-
-		password: z.string().min(6, "Password should be atleast 6 characters"),
-
-		confirmPassword: z.string().min(1, "Please confirm your password"),
-	})
-	.refine((data) => data.password === data.confirmPassword, {
+export const registerSchema = authSchema.refine(
+	(data) => data.password === data.confirmPassword,
+	{
 		message: "Passwords do not match",
 		path: ["confirmPassword"],
-	});
+	},
+);
