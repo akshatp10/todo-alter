5076a4c3ce47b4798bb452456ffb72cca693915c
akshatp10 <akshat@thealteroffice.com>
2026-08-17 12:57:07 +0530

    feat(TodoStore) : Implemented useTodoStore with addList functionality

--- client/store/todoStore.ts
+++ client/store/todoStore.ts
@@ -1,21 +1,7 @@
 import { sampleList } from "@/components/sampleData";
+import { TodoList } from "@/types/todo";
 import { create } from "zustand";
 import { persist } from "zustand/middleware";
-
-export type TaskStatus = "pending" | "completed";
-
-export interface Task {
-	id: number;
-	item_name: string;
-	status: TaskStatus;
-	tags: string[];
-}
-
-export interface TodoList {
-	id: number;
-	listName: string;
-	items: Task[];
-}
 
 type TodoStates = {
 	lists: TodoList[];
@@ -35,3 +21,33 @@ const initialState: TodoStates = {
 	lists: sampleList,
 	activeListId: sampleList[0]?.id ?? null,
 };
+
+const useTodoStore = create<TodoStore>()(
+	persist(
+		(set) => ({
+			...initialState,
+			addList: (name) =>
+				set((state) => {
+					const newId =
+						Math.max(0, ...state.lists.map((list) => list.id)) + 1;
+
+					const newList: TodoList = {
+						id: newId,
+						listName: name,
+						items: [],
+					};
+
+					return {
+						lists: [...state.lists, newList],
+						activeListId: newId,
+					};
+				}),
+			addTask: () => {},
+			toggleTask: () => {},
+			setActiveList: () => {},
+		}),
+		{
+			name: "todo-storage",
+		},
+	),
+);
