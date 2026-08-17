b65ecf51c8412b0f8254f483c2987a71fbb89651
akshatp10 <akshat@thealteroffice.com>
2026-08-17 14:59:30 +0530

    feat(Working List) : Implemented todoStore to main working component

--- client/app/home/page.tsx
+++ client/app/home/page.tsx
@@ -3,13 +3,9 @@
 import SideBarHome from "@/components/homepage/sidebar";
 import ListStatsComponent from "@/components/homepage/statisticsComponent";
 import WokringListComponent from "@/components/homepage/workingList";
-import { useState } from "react";
-import { sampleList } from "../../components/sampleData"
 
 
 export default function HomePage() {
-
-    const [currentList, setCurrentList] = useState(1)
 
     return (
         <>
@@ -18,7 +14,7 @@ export default function HomePage() {
                     <SideBarHome />
                 </div>
                 <div className="flex flex-col w-2/3 p-4">
-                    <WokringListComponent listDetails={sampleList[currentList - 1]} />
+                    <WokringListComponent />
                 </div>
                 <div className="flex flex-col w-1/3 border-l border-gray-200 p-4">
                     <ListStatsComponent />
--- client/components/homepage/workingList.tsx
+++ client/components/homepage/workingList.tsx
@@ -3,15 +3,18 @@
 import { Pencil, Plus } from "lucide-react";
 import NewListItemComponent from "./newList";
 import { useState } from "react";
-import AccordianComponent from "./accordianComponent";
+import useTodoStore from "@/store/todoStore";
+import { TodoList } from "@/types/todo";
 
-export default function WokringListComponent({ listDetails }: any) {
+export default function WokringListComponent() {
     const [newItem, setNewItem] = useState(false)
     const handleClickNewItem = () => {
         setNewItem(true)
     }
 
-    const listDetail = listDetails
+    const { lists, activeListId, addTask, toggleTask } = useTodoStore();
+
+    const currentList: TodoList = lists.find((list) => (list.id === activeListId))!;
 
     return (
         <>
@@ -21,17 +24,20 @@ export default function WokringListComponent({ listDetails }: any) {
 
             {/* Top Title and New Item */}
             <div className="flex w-full justify-between p-3">
-                <div className="text-3xl font-bold flex gap-4 items-center"> {listDetail.listName} <Pencil width={20} className="mt-1 text-gray-500" /></div>
+                <div className="text-3xl font-bold flex gap-4 items-center"> {currentList?.listName} <Pencil width={20} className="mt-1 text-gray-500" /></div>
                 <button className="bg-black text-white px-4 cursor-pointer flex justify-center items-center gap-2 rounded-md" onClick={handleClickNewItem}>
                     <Plus className="w-3.5" /> New Task
                 </button>
             </div>
 
             {/* Showing the list items */}
-            {listDetail.items.map((item: any) => (
+            {currentList?.items?.length > 0 ? currentList.items.map((item: any) => (
                 <div className="flex w-full gap-2 py-10 border-b border-gray-200" key={item.id}>
                     <div className="w-[5%] flex justify-end">
-                        <input type="checkbox" name="checkTask" defaultChecked={item.status === "completed"} />
+                        <input type="checkbox" name="checkTask"
+                            checked={item.status === "completed"}
+                            onChange={() => toggleTask(item.id)}
+                        />
                     </div>
                     <div className="flex flex-col w-[95%]">
                         {item.item_name}
@@ -44,9 +50,10 @@ export default function WokringListComponent({ listDetails }: any) {
                         </div>
                     </div>
                 </div>
-            ))}
-
-            {/* <AccordianComponent /> */}
+            )) : <div className="flex items-center justify-center py-16 text-sm text-gray-400 flex-col">
+                <p className="text-2xl text-black">No Tasks added</p>
+                <p>Add tasks to the list</p>
+            </div>}
         </>
     );
 }
