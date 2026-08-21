fda384a89f4d729295f5bba034c21ce1579e9310
akshatp10 <akshat@thealteroffice.com>
2026-08-21 18:03:31 +0530

    fix(AuthSchema) : Added more validation to password field

--- client/schema/authFormSchema.ts
+++ client/schema/authFormSchema.ts
@@ -1,5 +1,17 @@
 import { z } from "zod";
 
+const passwordSchema = z
+	.string()
+	.min(6, "Password must be at least 6 characters")
+	.regex(/[0-9]/, "Password needs at least one number")
+	.regex(
+		/[!@#$%^&*(),.?":{}|<>_\-+=/\\[\];'`~]/,
+		"Password needs at least one special character",
+	)
+	.regex(/[A-Z]/, "Password needs at least one uppercase letter")
+	.regex(/[a-z]/, "Password needs at least one lowercase letter")
+	.regex(/^\S*$/, "Password should not have spaces");
+
 const authSchema = z.object({
 	email: z.email("Please enter a valid email address"),
 
@@ -9,9 +21,9 @@ const authSchema = z.object({
 		.min(2, "Name must be at least 2 characters")
 		.max(50, "Name cannot exceed 50 characters"),
 
-	password: z.string().min(6, "Password should be at least 6 characters"),
+	password: passwordSchema,
 
-	confirmPassword: z.string().min(1, "Please confirm your password"),
+	confirmPassword: passwordSchema,
 });
 
 export const loginSchema = authSchema.omit({
