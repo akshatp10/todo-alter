c1ee06dd3589fdb0bfcea525473a2382f68e778f
akshatp10 <akshat@thealteroffice.com>
2026-08-24 11:39:56 +0530

    refactor(Statistics) : refactored the code to reduce redundancy

--- client/components/homepage/statisticsComponent.tsx
+++ client/components/homepage/statisticsComponent.tsx
@@ -75,21 +75,20 @@ export default function ListStatsComponent() {
             </div>
 
             <div className="shrink-0">
-                {publicAccess ?
-                    <div className="flex flex-col py-4 border-t border-gray-200 gap-2">
-                        <span className="font-bold text-gray-500 text-md">PUBLIC ACCESS</span>
-                        <div className="bg-gray-200 text-xs p-2 max-h-20 rounded-md">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sit natus atque amet, explicabo aspernatur eius temporibus odio mollitia omnis, velit, vel nostrum. Impedit magni dolores autem, veniam laudantium neque accusamus.</div>
-                        <button className="mx-auto w-full border py-1 rounded-md border-gray-200 cursor-pointer" onClick={() => {
-                            navigator.clipboard.writeText("Copying the data to clipboard")
-                        }}>Copy Public Link</button>
-                        <button className="mx-auto w-full text-red-500 bg-red-50 border py-1 rounded-md border-gray-200 cursor-pointer" onClick={() => { setPublicAccess(false) }}>Revoke Access</button>
-                    </div>
-                    :
-                    <div className="flex flex-col py-4 border-t border-gray-200 gap-2">
-                        <span className="font-bold text-gray-500 text-md">PUBLIC ACCESS</span>
-                        <button className="mx-auto w-full text-green-500 bg-green-50 border py-1 rounded-md border-gray-200 cursor-pointer" onClick={() => { setPublicAccess(true) }}>Give Public Access</button>
-                    </div>
-                }
+                <div className="flex flex-col py-4 border-t border-gray-200 gap-2">
+                    <span className="font-bold text-gray-500 text-md">PUBLIC ACCESS</span>
+                    {publicAccess &&
+                        <>
+                            <div className="bg-gray-200 text-xs p-2 max-h-20 rounded-md">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sit natus atque amet, explicabo aspernatur eius temporibus odio mollitia omnis, velit, vel nostrum. Impedit magni dolores autem, veniam laudantium neque accusamus.</div>
+                            <button className="mx-auto w-full border py-1 rounded-md border-gray-200 cursor-pointer" onClick={() => {
+                                navigator.clipboard.writeText("Copying the data to clipboard")
+                            }}>Copy Public Link</button>
+                        </>
+                    }
+                    <button className={`mx-auto w-full ${publicAccess ? "text-red-500 bg-red-50" : "text-green-500 bg-green-50"} border py-1 rounded-md border-gray-200 cursor-pointer`} onClick={() => { setPublicAccess(!publicAccess) }}>
+                        {publicAccess ? "Revoke Access" : "Give Public Access"}
+                    </button>
+                </div>
             </div>
         </>
     );
