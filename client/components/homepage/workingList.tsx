932633bc83ba8d3ce8841282b3d4d4048886dadb
akshatp10 <akshat@thealteroffice.com>
2026-08-25 14:39:37 +0530

    refactor(workingList) : simplifying the conditional rendering

--- client/components/homepage/workingList.tsx
+++ client/components/homepage/workingList.tsx
@@ -25,6 +25,8 @@ export default function WokringListComponent() {
 
 
     const currentList = lists[activeListId!]
+    const items = currentList.items ?? [];
+
     const [newTitle, setNewTitle] = useState<string>("")
 
     const handleUpdateTitle = () => {
@@ -76,7 +78,7 @@ export default function WokringListComponent() {
 
             {/* Showing the list items */}
             {
-                currentList.items.length > 0 ? currentList.items.map((item: Task) => (
+                items.length > 0 ? items.map((item: Task) => (
                     <div className="flex w-full gap-2 py-10 border-b border-gray-200" key={item.id}>
                         <div className="shrink-0">
                             <input type="checkbox" name="checkTask"
